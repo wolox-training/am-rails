ccfab15fee075d6931f194189d11ec50f2f68cf4
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-24 14:10:56 -0500

    Add TODO in CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # protect_from_forgery with: :exception
+
+  # TODO: To revise when we start active admin or when we have an api_contrller
   protect_from_forgery with: :null_session
+  # protect_from_forgery with: :exception
 end
