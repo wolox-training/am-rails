aee050fe6d69860e41019c75106e48ce5eacb755
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-25 10:27:49 -0500

    Add validation of fields in user model PR9

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+  validates :first_name, :last_name, presence: true
 end
