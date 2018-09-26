c686e7a762ead73ce807c4d1c1994d0e1bf95318
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-26 09:37:58 -0500

    Remove TODO comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # TODO: To revise when we start active admin or when we have an api_contrller
-  # protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery prepend: true
 end
