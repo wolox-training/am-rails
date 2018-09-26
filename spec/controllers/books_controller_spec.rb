5bba1729bed7e487422cb2b54af66e378a62bc98
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-26 15:57:16 -0500

    Structuring the test for book controller

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe BooksController, type: :controller do
+shared_context 'Authenticated User' do
+  let(:user) { create(:user) }
+  let!(:access_data) { AuthenticableEntity.generate_access_token(user) }
+  let!(:access_token) { access_data[:token] }
+  let!(:renew_id) { access_data[:renew_id] }
 
+  before do
+    request.headers['Authorization'] = access_token
+  end
+end
+
+describe BooksController do
+  include_context 'Authenticated User'
+
+  describe 'GET #index' do
+    # context 'When fetching all the books' do
+    #   let!(:rents) { create_list(:book, 3) }
+    #   before do
+    #     get :index
+    #   end
+
+      it 'responses with all the books json' do
+    #     expected = ActiveModel::Serializer::CollectionSerializer.new(
+    #       books, each_serializer: BookSerializer
+    #     ).to_json
+    #     expect(response_body.to_json) =~ JSON.parse(expected)
+    #   end
+    end
+  end
 end
