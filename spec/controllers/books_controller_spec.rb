bfbafbb1608dc9516cb7cca28cb6863bcabdf289
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-27 08:55:40 -0500

    Spec with non authenticated user ready

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
 describe BooksController do
-  include_context 'Authenticated User'
-
   describe 'GET #index' do
-    it 'responses with all the books json' do
+    before(:each) do
+      get :index
+    end
 
+    context 'without authentication' do
+      it 'responses with 401' do
+        expect(response.status).to eq(401)
+      end
+
+      it 'return a login error message' do
+        error_message = {
+          errors: ['You need to sign in or sign up before continuing.']
+        }.to_json
+        expect(response.body).to eq(error_message)
+      end
+    end
+
+    context 'with authentication' do
+      let(:resource) { create(:user) }
+      before(:each) do
+        request.headers.merge! resource.create_new_auth_token
+        get :index
+      end
+
+      it 'responses with 200' do
+      end
     end
   end
 end
--- spec/support/user_auth_shared_context.rb
+++ spec/support/user_auth_shared_context.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 shared_context 'Authenticated User' do
   let(:user) { create(:user) }
-  let!(:access_data) { AuthenticableEntity.generate_access_token(user) }
+  let!(:headers) { get :sign_in, params: user }
   let!(:access_token) { access_data[:token] }
   let!(:renew_id) { access_data[:renew_id] }
 
