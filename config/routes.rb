a12ff40f566836b269c623656e0a3270404ed519
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-27 11:19:05 -0500

    Change get for resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
     # devise_for :users  Commented out because devise is not used for login
     mount_devise_token_auth_for 'User', at: 'auth'
 
-    get '/books/', to: 'books#index'
-    get '/books/:id', to: 'books#show'
+    resources :books, only: [:index, :show]
     root to: 'books#index'
   end
 end
