a007e0ae35f9864d881656080265218b2fa8e2d9
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-28 17:14:19 -0500

    Add create route for rent

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -1,7 +1,14 @@
 module Api
   module V1
     class RentsController < ApiController
-      def create; end
+      def create
+        @rent = Rent.new(rent_params)
+        if @rent.save
+          render json: @rent, status: :created
+        else
+          render json: { error: @rent.errors.messages }
+        end
+      end
 
       def index_user
         @rents = Rent.where user_id: params[:user_id].to_i
@@ -11,6 +18,12 @@ module Api
       def index_book
         @rents = Rent.where book_id: params[:book_id].to_i
         render_paginated @rents
+      end
+
+      private
+
+      def rent_params
+        params.require(:rent).permit(:from_date, :to_date, :user_id, :book_id)
       end
     end
   end
