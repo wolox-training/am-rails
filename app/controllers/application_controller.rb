07c1def1c51b42b5a5793870524a254aca7e49ad
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-09-28 11:35:40 -0500

    Fix PR 12 comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, except: [:auth]
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
 end
