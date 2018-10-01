6dcfaf5694be164e80309482e337b58d7f4e0094
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-01 17:06:25 -0500

    Fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     # devise_for :users  Commented out because devise is not used for login
     mount_devise_token_auth_for 'User', at: 'auth'
 
-    resources :books, only: %i[index, show]
+    resources :books, only: %i[index show]
 
     resources :users do
       resources :rents, only: %i[index create]
