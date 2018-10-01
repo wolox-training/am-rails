15f32f6d9eb8cc6a7b09a71ba44f50f0f20aa209
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-01 16:25:12 -0500

    Starting with i18n

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -5,6 +5,11 @@ module Api
 
       protect_from_forgery with: :null_session
       before_action :authenticate_api_v1_user!
+      before_action :set_locale
+
+      def set_locale
+        I18n.locale = current_api_v1_user.locale || I18n.default_locale
+      end
     end
   end
 end
