23535d000e1d10da5100aba69827613ccba8a414
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-02 15:30:06 -0500

    ActiveAdmin support

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class ApiController < ApplicationController
       include Wor::Paginate
+      include DeviseTokenAuth::Concerns::SetUserByToken
 
       protect_from_forgery with: :null_session
       before_action :authenticate_api_v1_user!
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
-
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
