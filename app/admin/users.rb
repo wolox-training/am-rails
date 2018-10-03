3359bc7b1366fcec0ac30e822486569fa94bb5de
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-03 14:37:30 -0500

    Adding the CRUD configuration for ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,14 +1,4 @@
 ActiveAdmin.register User do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params %i[first_name last_name email provider uid locale tokens
+                   remember_created_at encrypted_password]
 end
