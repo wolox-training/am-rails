5d502d5c031298935920b582991997f925a3ca20
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-03 11:52:50 -0500

    Fix Rubocop offense

--- spec/factories/rent_factory.rb
+++ spec/factories/rent_factory.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :rent do
     user_id { create(:user).id }
     book_id { create(:book).id }
-    from_date { Faker::Date.between(1.year.ago, 1.year.from_now)}
+    from_date { Faker::Date.between(1.year.ago, 1.year.from_now) }
     to_date { Faker::Date.forward(1000) }
   end
 end
