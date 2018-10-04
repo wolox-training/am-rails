0f728993f145a6a91498833cf5e68a14043c36bc
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-04 09:27:13 -0500

    Add the specs for book suggestions controller

--- spec/controllers/book_suggestion_controller_spec.rb
+++ spec/controllers/book_suggestion_controller_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 describe Api::V1::BookSuggestionsController do
+  describe 'POST #create with authentication' do
+    subject(:response) do
+      post :create, params: {
+        book_suggestion: attributes_for(:book_suggestion, user_id: user.id)
+      }
+    end
+
+    include_context 'authenticated user'
+
+    it 'responses with :created status code' do
+      is_expected.to have_http_status(:created)
+    end
+
+    it 'contains the user id setted' do
+      expect(json_to_hash(subject.body)['user_id']).to_not eq nil
+    end
+  end
+
+  describe 'without authentication' do
+    subject(:response) do
+      post :create, params: {
+        book_suggestion: attributes_for(:book_suggestion)
+      }
+    end
+
+    it 'responses with :created status code' do
+      is_expected.to have_http_status(:created)
+    end
+
+    it 'contains the user_id setted to null' do
+      expect(json_to_hash(subject.body)['user_id']).to eq nil
+    end
+  end
 end
