9a061f24e92dab6fb3924e5b2867a77da3300988
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-04 16:55:44 -0500

    Delivering the json with the requested data

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -4,14 +4,28 @@ class OpenLibraryService
   include HTTParty
   base_uri 'openlibrary.org/api/books'
 
-  def self.book_info(isbn)
-    options = {
+  def book_info(isbn)
+    isbn_code = "ISBN#{isbn}"
+    response = self.class.get('', options(isbn_code))[isbn_code]
+
+    {
+      ISBN: isbn,
+      title: response['title'],
+      subtitle: response['subtitle'],
+      number_of_pages: response['number_of_pages'],
+      authors: response['authors'].map { |data| data['name'] }
+    }
+  end
+
+  private
+
+  def options(isbn)
+    {
       query: {
-        bibkeys: "ISBN:#{isbn}",
+        bibkeys: isbn,
         format: 'json',
         jscmd: 'data'
       }
     }
-    get('', options).body
   end
 end
