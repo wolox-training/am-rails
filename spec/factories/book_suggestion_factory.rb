80b99a903c78097f7cac9b1452ce0ff32ba90181
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-04 09:27:13 -0500

    Add the specs for book suggestions controller

---
+++ spec/factories/book_suggestion_factory.rb
@@ -0,0 +1,13 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :book_suggestion do
+    title     { Faker::Book.title }
+    author    { Faker::Book.author }
+    link      { Faker::Internet.url }
+    synopsis  { Faker::FamousLastWords.last_words }
+    publisher { Faker::Book.publisher }
+    year      { Faker::Number.between(1900, 2018) }
+    user_id   { nil }
+  end
+end
