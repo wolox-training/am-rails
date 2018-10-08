401cac27d5ba704e02841515aa1fcdb0c2bfe34a
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-08 11:40:35 -0500

    Refactor model fetch

--- app/jobs/rent_validation_job.rb
+++ app/jobs/rent_validation_job.rb
@@ -1,7 +1,7 @@
 class RentValidationJob < ApplicationJob
   def perform
     rents = Rent.where to_date: Time.zone.today
-    rents.each do |rent|
+    rents.find_each do |rent|
       RentMailer.with(rent_id: rent.id).rent_expiry.deliver_later
     end
   end
