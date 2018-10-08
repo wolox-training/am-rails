dcdd08acd2dad8074667d3ae011f489131954611
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-08 08:48:57 -0500

    Fix PR23 comments

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -10,6 +10,20 @@ module Api
         books = Book.all
         render_paginated books
       end
+
+      def info
+        isbn = params[:isbn]
+        if isbn.nil?
+          render json: { error: 'Please provide an ISBN code' }
+          return
+        end
+
+        book = OpenLibraryService.new(isbn).book_info
+        render json: book
+      rescue RuntimeError => e
+        render json: { error: e.to_s }
+      else
+      end
     end
   end
 end
