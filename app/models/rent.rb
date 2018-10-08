85d7f3d180d332263e4281e6014713a01143a5f9
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-08 11:38:45 -0500

    Adding custom validators

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,6 +1,9 @@
 class Rent < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :user
   belongs_to :book
 
+  validates_with RentDatesValidator, RentFromUniquenessValidator
   validates :user_id, :book_id, :from_date, :to_date, presence: true
 end
