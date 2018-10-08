120629a92bdae9a27a79a8c859708ea97fe033f3
Alejandro Ochoa <alejandro.ochoa@wolox.co>
2018-10-08 08:48:57 -0500

    Fix PR23 comments

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,31 +1,50 @@
-require 'json'
-
 class OpenLibraryService
   include HTTParty
-  base_uri 'openlibrary.org/api/books'
+  base_uri ENV['API_BOOK_URL']
 
-  def book_info(isbn)
-    isbn_code = "ISBN#{isbn}"
-    response = self.class.get('', options(isbn_code))[isbn_code]
+  FORMAT = 'json'.freeze
+  JSCMD = 'data'.freeze
 
-    {
-      ISBN: isbn,
-      title: response['title'],
-      subtitle: response['subtitle'],
-      number_of_pages: response['number_of_pages'],
-      authors: response['authors'].map { |data| data['name'] }
+  # Keys
+  TITLE = 'title'.freeze
+  SUBTITLE = 'subtitle'.freeze
+  N_PAGES = 'number_of_pages'.freeze
+  AUTHORS = 'authors'.freeze
+
+  def initialize(isbn)
+    @isbn = isbn
+    @options = {
+      query: {
+        bibkeys: @isbn,
+        format: FORMAT,
+        jscmd: JSCMD
+      }
     }
+  end
+
+  def book_info
+    response = retrieve_from_api
+    format_response response
   end
 
   private
 
-  def options(isbn)
+  def retrieve_from_api
+    response = self.class.get('', @options)
+    raise "No book found with code #{@isbn}" if response.body == '{}'
+
+    response
+  end
+
+  def format_response(response)
+    response = response[@isbn]
+
     {
-      query: {
-        bibkeys: isbn,
-        format: 'json',
-        jscmd: 'data'
-      }
+      ISBN: @isbn,
+      title: response[TITLE],
+      subtitle: response[SUBTITLE],
+      number_of_pages: response[N_PAGES],
+      authors: response[AUTHORS].map { |data| data['name'] }
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
     # devise_for :users  Commented out because devise is not used for login
     mount_devise_token_auth_for 'User', at: 'auth'
 
-    resources :books, only: %i[index show]
+    resources :books, only: %i[index show] do
+      get 'info', on: :collection
+    end
 
     resources :users do
       resources :rents, only: %i[index create]
     end
 
     resources :book_suggestions, only: :create
-    resources :info, only: :show, controller: :open_library
   end
 end
